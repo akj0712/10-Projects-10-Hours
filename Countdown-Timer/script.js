31e124aeb8fa06fe2a3f57ebec8df5442f9612f8
Abhinav Kumar Jha <akj0712@duck.com>
2022-03-24 14:22:39 +0530

    Countdown Timer

---
+++ Countdown-Timer/script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newDate = "7 Dec 2022";
+
+function countdown() {
+    const newYearsDate = new Date(newDate);
+    const currentDate = new Date();
+    // * console.log(newYearsDate - currentDate);
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 60 / 60 / 24);
+    const hours = Math.floor(totalSeconds / 60 / 60) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+    // * console.log(days, hours, minutes, seconds);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// * Initial call
+countdown();
+
+setInterval(countdown, 1000);
